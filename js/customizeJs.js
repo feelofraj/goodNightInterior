c83a2893d06523c70f35ec8bf1578226179b634d
feelofraj <feelofraj@gmail.com>
2019-01-05 02:40:42 +0530

    adding enquiry

--- js/customizeJs.js
+++ js/customizeJs.js
@@ -30,6 +30,37 @@ $(document).ready(function () {
 		},10000);
 
     });
+
+$("#enquiryMail").submit(function (event) {
+
+    /* stop form from submitting normally */
+    event.preventDefault();
+
+    /* get the action attribute from the <form action=""> element */
+    var $form = $(this),
+        url = $form.attr('action');
+    var parameters = { name: $('#name').val(),email: $('#email').val(), message: $('#message').val(), telephone: $('#contact').val(), mailSubject: "Request from Good Night Interiors", toMail: "goodnightmosquito@gmail.com"};
+    $.ajax({
+        url: "https://script.google.com/macros/s/AKfycbxdQUSLUHH1g9V3qlYqzd3ZPCBXjIIkD_QOqAsNOEsyLNWRADXI/exec",
+        method: "POST",
+        data: parameters
+    })
+        .done(function (data) {
+
+            $('#enquiry-error-msg').css('color', 'green');
+            $('#enquiry-error-msg').html('Email sent successfully.');
+            location.reload();
+        })
+        .fail(function (jqXHR, textStatus, errorThrown) {
+
+            $('#error-msg').css('color', 'red');
+            $('#error-msg').html('Falied: Please try again later or contact us.');
+        });
+        setTimeout(function(){
+            location.reload();
+    },6000);
+
+});
 });
 
 function getMobileOperatingSystem(msgContent) {
